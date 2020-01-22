bab639cd6374a0cb160f645a583b43d402e00c68
Avital <avital.bor@campus.technion.ac.il>
2020-01-22 17:37:12 +0200

    enter parking + functions ezer

--- ParkingLot.h
+++ ParkingLot.h
@@ -68,7 +68,9 @@ namespace MtmParkingLot {
         friend ostream& operator<<(ostream& os, const ParkingLot& parkingLot);
 
 
-        Vehicle& getVehicleFromArray( UniqueArray <Vehicle,Compare> wanted_array,LicensePlate licensePlate);
+        Vehicle* getVehicleFromLicensePlate(LicensePlate licensePlate);
+        bool  checkIfExistsSpot(VehicleType vehicleType);
+        void  enterVehicleToparking(Vehicle &register_vehicle, VehicleType vt);
 
         // execptions class
 
@@ -94,75 +96,79 @@ namespace MtmParkingLot {
 
 
     //mimush:
-    Vehicle&
+
+
+    Vehicle* MtmParkingLot::ParkingLot::getVehicleFromLicensePlate(LicensePlate licensePlate){
+        Vehicle new_vehicle(licensePlate);
+        Vehicle* exists =motorbikes_arr[new_vehicle];
+        if(exists!=NULL){
+            return exists;
+        }
+        exists= cars_arr[new_vehicle];
+        if(exists!=NULL){
+            return exists;
+        }
+        exists=handicapped_cars_arr[new_vehicle];
+        return exists;
+    }
+
+
+
 
     bool MtmParkingLot::ParkingLot:: checkIfExistsSpot(VehicleType vehicleType){
         if(vehicleType==MOTORBIKE){
-            return motorbikes.getCount() != motorbikes.getSize();
+            return motorbikes_arr.getCount() != motorbikes_arr.getSize();
         }
         if(vehicleType==HANDICAPPED){
-            if(handicappeds.getCount()!=handicappeds.getCount()){
+            if(handicapped_cars_arr.getCount()!=handicapped_cars_arr.getCount()){
                 return true;
             }
         }
 
-        return cars.getSize() != cars.getCount();
-
+        return cars_arr.getSize() != cars_arr.getCount();
     }
 
 
 
     void MtmParkingLot::ParkingLot:: enterVehicleToparking(Vehicle &register_vehicle, VehicleType vt){
         if(vt==MOTORBIKE){
-            motorbikes.insert(register_vehicle);
+            motorbikes_arr.insert(register_vehicle);
             //assert success
             return;
         }
         else if(vt==HANDICAPPED){
-            if(handicappeds.getCount()!=handicappeds.getSize()){
-                handicappeds.insert(register_vehicle);
+            if(handicapped_cars_arr.getCount()!=handicapped_cars_arr.getSize()){
+                handicapped_cars_arr.insert(register_vehicle);
                 return;
             }
         }
         //if we are here than the vt is handicappes or cars and in any case we should
         //insert the vehicle to the cars section
-        cars.insert(register_vehicle);
+        cars_arr.insert(register_vehicle);
 
     }
 
     ParkingLotUtils:: ParkingResult MtmParkingLot::ParkingLot:: enterParking(VehicleType vehicleType,
                                                                              LicensePlate licensePlate, Time entranceTime){
-        //bulid new_v
-        Vehicle v(vehicleType,licensePlate,entranceTime);
-        //chekc if existx in one of the unique arrays
-        Vehicle* exists= motorbikes[v];
-        ParkingSpot spot;
+        Vehicle new_vehicle(vehicleType,licensePlate,entranceTime);
+        //getVehicleFromArray(motorbikes_arr,licensePlate);
 
-        //shihpul kod, fix later
-        if(exists!=NULL){
+        Vehicle* exists= getVehicleFromLicensePlate(licensePlate);
+        ParkingSpot spot;
+        if(exists!=NULL){//vehicle already exists
             getParkingSpot(licensePlate,spot);
-            printEntryFailureAlreadyParked(cout,spot);
+            ParkingLotPrinter::printEntryFailureAlreadyParked(cout,spot);
             return VEHICLE_ALREADY_PARKED;
         }
-        exists=handicappeds[v];
-        if(exists!=NULL){
-            getParkingSpot(licensePlate,spot);
-            printEntryFailureAlreadyParked(cout,spot);
-            return VEHICLE_ALREADY_PARKED;
-        }
-        exists=cars[v];
-        if(exists!=NULL){
-            getParkingSpot(licensePlate,spot);
-            printEntryFailureAlreadyParked(cout,spot);
-            return VEHICLE_ALREADY_PARKED;
-        }
+
         //vehicle isn't parked already
         if(!checkIfExistsSpot(vehicleType)){
             ParkingLotPrinter::printEntryFailureNoSpot(cout);
             return NO_EMPTY_SPOT;
         }
-        enterVehicleToparking(v,vehicleType);
-        ParkingLotPrinter::printEntrySuccess(cout, v);
+
+        enterVehicleToparking(new_vehicle,vehicleType);
+        ParkingLotPrinter::printEntrySuccess(cout, new_vehicle);
         return SUCCESS;
     }
 
